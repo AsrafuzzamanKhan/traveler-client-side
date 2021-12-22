3d771081cccb2f95bccc51285290321595b04a9f
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2021-12-23 04:07:40 +0600

    banner

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Activities from '../../Activities/Activities/Activities';
 import Packages from '../../Packages/Packages/Packages';
-import Banner from '../Banner/Banner';
+// import Banner from '../Banner/Banner';
 import MiddlePart from '../MiddlePart/MiddlePart';
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
+            {/* <Banner></Banner> */}
             <MiddlePart></MiddlePart>
             <Packages></Packages>
             <Activities></Activities>
